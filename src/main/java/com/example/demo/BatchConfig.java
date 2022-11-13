273ec44618f1f555ced88327d63cbfbc35ab207b
RyoyaToba <ryoya.toba@rakus-partners.co.jp>
2022-11-13 16:16:48 +0900

    CSVへの出力はできた

--- src/main/java/com/example/demo/BatchConfig.java
+++ src/main/java/com/example/demo/BatchConfig.java
@@ -1,5 +1,8 @@
 package com.example.demo;
 
+import java.io.FileOutputStream;
+import java.nio.charset.StandardCharsets;
+
 import javax.persistence.EntityManagerFactory;
 import javax.sql.DataSource;
 
@@ -12,12 +15,17 @@ import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.item.database.JdbcCursorItemReader;
 import org.springframework.batch.item.database.builder.JdbcCursorItemReaderBuilder;
 import org.springframework.batch.item.file.FlatFileItemReader;
+import org.springframework.batch.item.file.FlatFileItemWriter;
 import org.springframework.batch.item.file.builder.FlatFileItemReaderBuilder;
+import org.springframework.batch.item.file.builder.FlatFileItemWriterBuilder;
 import org.springframework.batch.item.file.mapping.BeanWrapperFieldSetMapper;
+import org.springframework.batch.item.file.transform.DelimitedLineAggregator;
+import org.springframework.batch.item.file.transform.DelimitedLineTokenizer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.FileSystemResource;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 
 import com.example.demo.Entiry.Person;
@@ -53,7 +61,9 @@ public class BatchConfig {
 	// Jobの実装
 	@Bean
 	public Job testJob1() {
-		return jobBuilderFactory.get("testJob").incrementer(new RunIdIncrementer()).start(testStep1()).build();
+		return jobBuilderFactory.get("testJob")
+				.incrementer(new RunIdIncrementer())
+				.start(testStep1()).build();
 	}
 
 	// Stepの実装
@@ -63,22 +73,9 @@ public class BatchConfig {
 		return stepBuilderFactory.get("step1").<Person, Person>chunk(3) // chunk数の指定
 				.reader(jdbcCursorItemReaderSample()) // reader指定
 				.processor(sampleProcess()) // processorの指定
-				.writer(sampleWrite()).build(); // writerの指定
+				.writer(fileItemWriterSample()).build(); // writerの指定
 	}
 
-	// readerの部分 CSVファイルの読み込みを行っている
-	@Bean
-	public FlatFileItemReader<Person> fileReadSample() {
-		return new FlatFileItemReaderBuilder<Person>().name("testCSV").resource(new ClassPathResource("person.csv"))
-				.delimited().names(new String[] { "id", "name", "age" })
-				.fieldSetMapper(new BeanWrapperFieldSetMapper<Person>() {
-					{
-						setTargetType(Person.class);
-					}
-				}).build();
-	}
-
-	
 	// JdbcCursorItemReaderを利用してDB上の情報を読み込む
 	@Bean
 	public JdbcCursorItemReader<Person> jdbcCursorItemReaderSample() {
@@ -106,4 +103,19 @@ public class BatchConfig {
 	public SampleWrite<Person> sampleWrite() {
 		return new SampleWrite<Person>();
 	}
+	
+	// FlatFileItemWriterを利用してSCVファイルの作成
+	@Bean
+	public FlatFileItemWriter<Person> fileItemWriterSample(){
+		
+		DelimitedLineAggregator<Person> aggregator = new DelimitedLineAggregator<>();
+		aggregator.setDelimiter(DelimitedLineTokenizer.DELIMITER_COMMA);
+		
+		return new FlatFileItemWriterBuilder<Person>().name("fileItemReaderSample")
+														.resource(new FileSystemResource("src/main/resources/sample-data.csv"))
+														.lineAggregator(aggregator)
+														.encoding(StandardCharsets.UTF_8.name())
+														.build();
+	}
+	
 }
